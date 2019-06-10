8725a373cb1ebdd1ceaa1150bd82335c958a7cc0
dereknoblej <derek.noblej@gmail.com>
2019-06-10 16:09:14 -0500

    bonus 1 complete

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -11,7 +11,12 @@ public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
             PrintWriter out = res.getWriter();
-            out.println("Hello, World!");
+            String ByName = req.getParameter("name");
+            if(ByName == null){
+                out.println("Hello, World!");
+            } else {
+                out.println("Hello, " + ByName);
+            }
 
     }
 }
